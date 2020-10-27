b70c46ea008b2fc7775d6700e519361c74a77a4a
Kyle Hume <kylehumebusiness@gmail.com>
2020-10-27 12:36:51 -0400

    q1

--- jenkins/core/src/main/java/hudson/model/UpdateCenter.java
+++ jenkins/core/src/main/java/hudson/model/UpdateCenter.java
@@ -546,16 +546,8 @@ public class UpdateCenter extends AbstractModelObject implements Saveable, OnMas
         }
     }
 
-    private XmlFile getConfigFile() {
-        return new XmlFile(XSTREAM,new File(Jenkins.getInstance().root,
-                                    UpdateCenter.class.getName()+".xml"));
-    }
-
-    public List<Plugin> getAvailables() {
-        Map<String,Plugin> pluginMap = new LinkedHashMap<String, Plugin>();
-        for (UpdateSite site : sites) {
-            for (Plugin plugin: site.getAvailables()) {
-                final Plugin existing = pluginMap.get(plugin.name);
+    public void tryAddPluginToMap(Map<String,Plugin> pluginMap, Plugin plugin) {
+        final Plugin existing = pluginMap.get(plugin.name);
                 if (existing == null) {
                     pluginMap.put(plugin.name, plugin);
                 } else if (!existing.version.equals(plugin.version)) {
@@ -566,6 +558,18 @@ public class UpdateCenter extends AbstractModelObject implements Saveable, OnMas
                         pluginMap.put(altKey, plugin);
                     }
                 }
+    }
+
+    private XmlFile getConfigFile() {
+        return new XmlFile(XSTREAM,new File(Jenkins.getInstance().root,
+                                    UpdateCenter.class.getName()+".xml"));
+    }
+
+    public List<Plugin> getAvailables() {
+        Map<String,Plugin> pluginMap = new LinkedHashMap<String, Plugin>();
+        for (UpdateSite site : sites) {
+            for (Plugin plugin: site.getAvailables()) {
+                tryAddPluginToMap(pluginMap, plugin);
             }
         }
 
@@ -603,17 +607,7 @@ public class UpdateCenter extends AbstractModelObject implements Saveable, OnMas
         Map<String,Plugin> pluginMap = new LinkedHashMap<String, Plugin>();
         for (UpdateSite site : sites) {
             for (Plugin plugin: site.getUpdates()) {
-                final Plugin existing = pluginMap.get(plugin.name);
-                if (existing == null) {
-                    pluginMap.put(plugin.name, plugin);
-                } else if (!existing.version.equals(plugin.version)) {
-                    // allow secondary update centers to publish different versions
-                    // TODO refactor to consolidate multiple versions of the same plugin within the one row
-                    final String altKey = plugin.name + ":" + plugin.version;
-                    if (!pluginMap.containsKey(altKey)) {
-                        pluginMap.put(altKey, plugin);
-                    }
-                }
+                tryAddPluginToMap(pluginMap, plugin);
             }
         }
 
